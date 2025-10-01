3b9fc27fe3a47672a0dff9fc6c5673bf59ff7b87
abdulzaf <zafar.abdullah10@gmail.com>
2025-09-30 23:27:01 -0400

    cli update

--- neurocogdb/cli.py
+++ neurocogdb/cli.py
@@ -1,18 +1,21 @@
 import argparse
 import subprocess
+from importlib.resources import files
+import sys
 from neurocogdb.load.sync import sync_ddb
 from neurocogdb.config.paths import config_root_path, confirm_root_path
+
 
 def get_parser():
     parser = argparse.ArgumentParser(prog="neurocogdb")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     # config
-    config_parser = subparsers.add_parser("config", help="Set/View current configuration.")
+    config_parser = subparsers.add_parser(
+        "config", help="Set/View current configuration."
+    )
     config_parser.add_argument(
-        "--set",
-        action="store_true",
-        help="Set a new root path."
+        "--set", action="store_true", help="Set a new root path."
     )
 
     # sync
@@ -37,8 +40,10 @@ def run_command(args):
             sync_ddb()
 
         case "gui":
+            gui_path = files("neurocogdb.ddb").joinpath("gui.py")
+            # run Streamlit on the actual file location
             subprocess.run(
-                ["streamlit", "run", "neurocogdb/ddb/gui.py"],  # Command and its arguments as a list
+                [sys.executable, "-m", "streamlit", "run", str(gui_path)],
                 capture_output=True,
                 text=True,  # Decodes stdout and stderr as text
                 check=True,  # Raises CalledProcessError if the command returns a non-zero exit code
